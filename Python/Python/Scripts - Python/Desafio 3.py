8dd05e8d93ff2c2612eb49a49fc3f78a30f6d58c
Kauã Oliveira Seixas <Kos281@hotmail.com>
2023-10-04 11:46:54 -0300

    ReposiçãoDeArquivos
    RepondoArquivosPraMain

---
+++ Python/Python/Scripts - Python/Desafio 3.py
@@ -0,0 +1,4 @@
+N1= int(input('Digite um número'))
+N2= int(input('Digite outro número'))
+print ('A soma é',N1+N2)
+            
---
+++ Python/Python/Scripts - Python/Teste - 1.py
@@ -0,0 +1,4 @@
+nome=input ('Qual o seu nome?')
+idade=input ('Qual a sua idade?')
+cor=input ('Qual a sua cor favorita?')
+print (nome, idade, cor)
